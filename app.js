e291b0cdefaa597616cd8a0e4d3f44bcff918f8c
urvibhanushali06 <6urvibhanushali@gmail.com>
2021-02-23 14:55:28 +0530

    Hashing and Salting with bcrypt

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-var md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 
 const app = express();
 
-console.log(process.env.API_KEY);
-
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-mongoose.connect('mongodb://localhost:27017/userDB' , {useNewUrlParser:true, useUnifiedTopology: true } );
+mongoose.connect('mongodb://localhost:27017/userDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const userSchema = new mongoose.Schema({
     email: String,
@@ -39,36 +43,44 @@ app.get('/register', function (req, res) {
 });
 
 app.post('/register', function (req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password:md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets');
+            }
+        });
     });
-    newUser.save(function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('secrets');
-        }
-    });
+
 });
 
 app.post('/login', function (req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
-    User.findOne({ email: username }, function (err, foundUser) {
+    User.findOne({
+        email: username
+    }, function (err, foundUser) {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render('secrets');
-                }
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result === true) {
+                        res.render('secrets');
+                    }
+                });
             }
         }
     });
-    }); 
-    
+});
+
 
 app.listen(3000, function () {
     console.log("Server started at port 3000");
